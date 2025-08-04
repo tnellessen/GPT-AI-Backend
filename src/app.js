e81851a1d3dd3f89da1dc03046adfbec8d129405
tnellessen <tnellessen@gmail.com>
2025-08-04 08:59:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const intakeRoutes = require('./routes/intakeRoutes');
 const app = express();
 app.use(cors({
   // origin: 'https://gpt-ai-gilt.vercel.app/form',
-  origin: 'https://gpt-ai-gilt.vercel.app/form',
+  origin: 'https://gpt-ai-gilt.vercel.app',
   
   methods: ['Get','Post'],
   credentials: true,
